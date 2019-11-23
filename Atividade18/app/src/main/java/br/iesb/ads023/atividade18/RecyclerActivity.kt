a0810a2c75204ed94666903babc89ff307e57693
Leonardo Rodrigues <leonardorbmedeiros@gmail.com>
2019-11-22 22:54:29 -0200

    Adicionando Atividade18

---
+++ Atividade18/app/src/main/java/br/iesb/ads023/atividade18/RecyclerActivity.kt
@@ -0,0 +1,24 @@
+package br.iesb.ads023.atividade18
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler)
+        setTitle("Recycler")
+        val lista: List<String> = listOf<String>("Asa Sul", "Asa Norte", "Sudoeste", "Noroeste", "Lago Sul", "Lago Norte", "Aguas Claras", "Candangolandia", "Nucleo Bandeirantes", "Vicentes Pires", "Taguatinga", "Ceilandia", "Samambaia")
+        val recyclerView = findViewById(R.id.recycler) as RecyclerView
+        val viewAdapter = RecyclerAdapter(lista, this)
+        val viewManager = LinearLayoutManager(this)
+        recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+    }
+}
